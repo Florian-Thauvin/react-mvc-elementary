eb78d80aa8f5d083d38dc9e639fca891322f7cf7
Florian Thauvin <florian.thauvin@orange.fr>
2020-10-17 16:45:19 +0200

    Déplacement de l'intelligence du model

--- prometeus/src/view/common/complex/array/PrometeusWidgetTable.tsx
+++ prometeus/src/view/common/complex/array/PrometeusWidgetTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { IModelField } from '../../../../model/common/ModelField';
 
@@ -8,23 +8,52 @@ import CustomTableControler from './subelements/CustomTableControler';
 import CustomTableHeaders from './subelements/CustomTableHeaders';
 import CustomTableBody from './subelements/CustomTableBody';
 
-import {getDeltaAttributesNameFromModel} from '../../../../utils/FieldUtils';
+import { getDeltaAttributesNameFromModel } from '../../../../utils/FieldUtils';
+import { EConstraintsType } from '../../../../model/common/ModelConstraints';
+import isValid from '../../../../utils/validation/GenericValidation';
 
 export interface inputProps {
     model: Array<IModelField>;
     headers?: Array<string>;
 }
 
+export interface ITableItem{
+    model: IModelField,
+    isSelected: boolean,
+    errors: Array<EConstraintsType>,
+    isValid: boolean
+}
+
+function convertModelToTableItems(model: Array<IModelField>): Array<ITableItem>{
+    const converted: Array<ITableItem> = [];
+    
+    model.forEach((element: IModelField) => {
+        const errors: Array<EConstraintsType> = isValid(element);
+
+        console.log("Table errors : " + errors);
+
+        converted.push({
+            model: element,
+            isSelected: false,
+            errors: errors,
+            isValid: errors.length === 0
+        });
+    });
+
+    return converted;
+}
+
 function PrometeusWidgetTable(props: inputProps): JSX.Element {
-
     const { model, headers } = props;
-
     const displayHeaders: string[] = headers ? headers : getDeltaAttributesNameFromModel(model[0]);
+
+    const [values, setValues] = useState(convertModelToTableItems(model));
+    const [displayValues, setDisplayValues] = useState(values);
 
     return (
         <Table>
             <CustomTableHeaders headers = {displayHeaders}/>
-            <CustomTableBody model = {model} headers = {displayHeaders}/>
+            <CustomTableBody model = {displayValues} headers = {displayHeaders}/>
         </Table>
     );
 }
--- prometeus/src/view/common/complex/array/subelements/CustomTableBody.tsx
+++ prometeus/src/view/common/complex/array/subelements/CustomTableBody.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { defaultModelField, IModelField } from '../../../../../model/common/ModelField';
 
 import Table from '@material-ui/core/Table';
@@ -8,40 +8,13 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+
 import { getDeltaAttributes } from '../../../../../utils/FieldUtils';
-import isValid from '../../../../../utils/validation/GenericValidation';
-import { EConstraintsType } from '../../../../../model/common/ModelConstraints';
-import { valueToNode } from '@babel/types';
+import { ITableItem } from '../PrometeusWidgetTable';
 
 interface inputProps {
-    model: Array<IModelField>,
+    model: Array<ITableItem>,
     headers: Array<string>
-}
-
-interface ITableItem{
-    model: IModelField,
-    isSelected: boolean,
-    errors: Array<EConstraintsType>,
-    isValid: boolean
-}
-
-function convertModelToTableItems(model: Array<IModelField>): Array<ITableItem>{
-    const converted: Array<ITableItem> = [];
-    
-    model.forEach((element: IModelField) => {
-        const errors: Array<EConstraintsType> = isValid(element);
-
-        console.log("Table errors : " + errors);
-
-        converted.push({
-            model: element,
-            isSelected: false,
-            errors: errors,
-            isValid: errors.length === 0
-        });
-    });
-
-    return converted;
 }
 
 function getOrderedRows(element: IModelField, headers: Array<string>): Array<IModelField> {
@@ -59,13 +32,10 @@ function getOrderedRows(element: IModelField, headers: Array<string>): Array<IMo
 function CustomTableBody(props: inputProps): JSX.Element {
     const { model, headers } = props;
 
-    const [values, setValues] = useState(convertModelToTableItems(model));
-    const [displayValues, setDisplayValues] = useState(values);
-
     return (
         <TableBody>
             {
-                displayValues.map((element: ITableItem) => {
+                model.map((element: ITableItem) => {
                     const { model, isValid } = element;
                     return (
                         <TableRow key={model.key}> 
