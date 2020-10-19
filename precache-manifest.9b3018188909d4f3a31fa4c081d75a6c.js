26ea29fa678253528589b81a0915cd93bee6436d
Florian Thauvin <florian.thauvin@orange.fr>
2020-10-19 09:12:39 +0200

    Updates

---
+++ precache-manifest.9b3018188909d4f3a31fa4c081d75a6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccd00b55afd2f6e3ad54f8adcd42152",
+    "url": "/react-mvc-elementary/index.html"
+  },
+  {
+    "revision": "0979f273953472699369",
+    "url": "/react-mvc-elementary/static/js/2.51327fd1.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/react-mvc-elementary/static/js/2.51327fd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a106a1f4efa8c51a67",
+    "url": "/react-mvc-elementary/static/js/main.01e42810.chunk.js"
+  },
+  {
+    "revision": "86c3f6a00633ebe91342",
+    "url": "/react-mvc-elementary/static/js/runtime-main.22526c53.js"
+  }
+]);
